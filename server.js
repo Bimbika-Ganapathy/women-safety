fb74195d72ccc25f68fcd87a80ddd85c8a53d1cb
Bimbika-Ganapathy <bimbikag@gmail.com>
2025-08-02 15:31:48 +0530

    Initial commit - backend setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
+
+// Sample Route
+app.get('/', (req, res) => {
+  res.send('Women Safety API is running...');
+});
+
+// Import your routes here
+// const userRoutes = require('./routes/userRoutes');
+// app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
