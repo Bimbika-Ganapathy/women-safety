b63c3551b33fe6c2599a844f612d3d8f60c3bba1
Bimbika-Ganapathy <bimbikag@gmail.com>
2025-08-02 16:19:57 +0530

    Added user authentication (register & login)

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const authRoutes = require('./routes/auth');
 
 dotenv.config();
 
@@ -10,6 +11,10 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
